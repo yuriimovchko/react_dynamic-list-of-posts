0dd1c0a999b7ea5e0ee0303707b7fe3157d5e6be
yuriimovchko <movchko.yuriy@gmail.com>
2020-09-25 09:34:14 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9b9b912321e3d315fc8f540d885eee7f.js"
+  "./precache-manifest.6454436845b0e6c3f608e0219818ceed.js"
 );
 
 self.addEventListener('message', (event) => {
